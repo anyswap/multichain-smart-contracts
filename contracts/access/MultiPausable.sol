d308a2256889270b9b4bb58fbd187694fff48484
bohaos <bohaos@multichain.org>
2022-03-31 11:30:13 +0800

    update paused event

--- contracts/access/MultiPausable.sol
+++ contracts/access/MultiPausable.sol
@@ -7,16 +7,15 @@ abstract contract PausableControl {
     bool private _pausedAll;
     address public _admin;
 
-    event Paused(bytes32 role);
-    event Unpaused(bytes32 role);
+    event Paused(bytes32 role, bool flag);
     event PausedAll(bool flag);
 
-    constructor (address admin){
-        _admin=admin;
+    constructor(address admin) {
+        _admin = admin;
     }
 
-    modifier onlyAdmin(){
-        require(msg.sender==_admin,"PausableControl:not admin");
+    modifier onlyAdmin() {
+        require(msg.sender == _admin, "PausableControl:not admin");
         _;
     }
 
@@ -27,69 +26,79 @@ abstract contract PausableControl {
     }
 
     modifier whenPaused(bytes32 role) {
-        require(pausedAll()||paused(role), "PausableControl: paused");
+        require(pausedAll() || paused(role), "PausableControl: paused");
         _;
     }
 
-    function changeAdmin(address admin_) external onlyAdmin{
-        require(_admin!=admin_,"PausableControl:admin not change");
-        _admin=admin_;
+    function changeAdmin(address admin_) external onlyAdmin {
+        require(_admin != admin_, "PausableControl:admin not change");
+        _admin = admin_;
     }
 
     function paused(bytes32 role) public view virtual returns (bool) {
         return _pausedRoles[role];
     }
 
-    function pausedAll() public view virtual returns (bool){
+    function pausedAll() public view virtual returns (bool) {
         return _pausedAll;
     }
 
-    function pause(bytes32 role) external onlyAdmin whenNotPaused(role){
-        _pause(role);
+    function pause(bytes32 role) external onlyAdmin whenNotPaused(role) {
+        _pause(role, true);
     }
 
-    function _pause(bytes32 role) internal{
-        _pausedRoles[role]=true;
-        emit Paused(role);
+    function _pause(bytes32 role, bool flag) internal {
+        _pausedRoles[role] = flag;
+        emit Paused(role, flag);
     }
 
-    function _setPauseAll(bool flag) internal{
-        _pausedAll=flag;
+    function _setPauseAll(bool flag) internal {
+        _pausedAll = flag;
         emit PausedAll(flag);
     }
 
-    function unPause(bytes32 role) external onlyAdmin whenPaused(role){
-        _pausedRoles[role]=false;
-        emit Unpaused(role);
+    function unPause(bytes32 role) external onlyAdmin whenPaused(role) {
+        _pause(role, false);
     }
 
-    function setPauseAll(bool flag) external onlyAdmin{
-        require(pausedAll()!=flag, "PausableControl: _pausedAll not change");
+    function setPauseAll(bool flag) external onlyAdmin {
+        require(pausedAll() != flag, "PausableControl: _pausedAll not change");
         _setPauseAll(flag);
     }
 }
 
-contract test is PausableControl{
-
-    bytes32 public constant Native_Paused_ROLE = keccak256("Native_Paused_ROLE");
-    bytes32 public constant Underlying_Paused_ROLE = keccak256("Underlying_Paused_ROLE");
+contract test is PausableControl {
+    bytes32 public constant Native_Paused_ROLE =
+        keccak256("Native_Paused_ROLE");
+    bytes32 public constant Underlying_Paused_ROLE =
+        keccak256("Underlying_Paused_ROLE");
     bytes32 public constant Token_Paused_ROLE = keccak256("Token_Paused_ROLE");
 
-    event Test(bytes32 role,uint256 number);
-    constructor (address admin) PausableControl(admin){
-        _pause(Native_Paused_ROLE);
+    event Test(bytes32 role, uint256 number);
+
+    constructor(address admin) PausableControl(admin) {
+        _pause(Native_Paused_ROLE, true);
         // _setPauseAll(true);
     }
 
-    function test_Native_Paused_ROLE(uint256 number) public whenNotPaused(Native_Paused_ROLE){
-        emit Test(Native_Paused_ROLE,number);
+    function test_Native_Paused_ROLE(uint256 number)
+        public
+        whenNotPaused(Native_Paused_ROLE)
+    {
+        emit Test(Native_Paused_ROLE, number);
     }
 
-    function test_Underlying_Paused_ROLE(uint256 number) public whenNotPaused(Underlying_Paused_ROLE){
-        emit Test(Underlying_Paused_ROLE,number);
+    function test_Underlying_Paused_ROLE(uint256 number)
+        public
+        whenNotPaused(Underlying_Paused_ROLE)
+    {
+        emit Test(Underlying_Paused_ROLE, number);
     }
 
-    function test_Token_Paused_ROLE(uint256 number) public whenNotPaused(Token_Paused_ROLE){
-        emit Test(Token_Paused_ROLE,number);
+    function test_Token_Paused_ROLE(uint256 number)
+        public
+        whenNotPaused(Token_Paused_ROLE)
+    {
+        emit Test(Token_Paused_ROLE, number);
     }
 }
