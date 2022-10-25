74557becaedfc6fa2315a46c89472a6f603c8925
Igor <43533945+Overseven@users.noreply.github.com>
2022-10-25 14:24:56 +0300

    add comment about xcm weight

--- scripts/generate-xcm-transfer-data.js
+++ scripts/generate-xcm-transfer-data.js
@@ -1,6 +1,6 @@
 async function main() {
     const transferAmount = '100000000000000000000'; // 100 xcBNB
-    const weight = 1000000000;
+    const weight = 1000000000; // there may be different values for different parachains
     const parachainId = '0x00000007DB'; // Equilibrium Parachain
     const recipientAddress = "4829b1e41449bd2cc7f04df856052f4d439f2f3e7f346c9702b94928ddf04707"; // Public key format
     const parents = 1; // Up to relay-chain (Polkadot/Kusama chain)
